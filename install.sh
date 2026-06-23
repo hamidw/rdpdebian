0b8dd5798cf01ad30f17ed7e33076b537f4eed33
hamidw <130257828+hamidw@users.noreply.github.com>
2026-06-23 15:48:29 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,58 +4,58 @@ set -e
 
 OS_ID=$(source /etc/os-release && echo "$ID")
 
-# # Hapus branding provider
+# Hapus branding provider
 
-# rm -rf /etc/update-motd.d/10-help-text
+rm -rf /etc/update-motd.d/10-help-text
 
-# #rm -rf /etc/profile.d/99-idcloudhost-motd.sh
-# #rm -rf /etc/profile.d/motd.sh
+rm -rf /etc/profile.d/99-idcloudhost-motd.sh
+rm -rf /etc/profile.d/motd.sh
 
-# # Hapus banner login
+# Hapus banner login
 
-# echo "" > /etc/issue
-# echo "" > /etc/issue.net
+echo "" > /etc/issue
+echo "" > /etc/issue.net
 
-# # Disable SSH banner safely
-# if grep -q "^Banner" /etc/ssh/sshd_config; then
-#     sed -i 's|^Banner.*|Banner none|' /etc/ssh/sshd_config
-# else
-#     echo "Banner none" >> /etc/ssh/sshd_config
-# fi
+# Disable SSH banner safely
+if grep -q "^Banner" /etc/ssh/sshd_config; then
+    sed -i 's|^Banner.*|Banner none|' /etc/ssh/sshd_config
+else
+    echo "Banner none" >> /etc/ssh/sshd_config
+fi
 
-# # Download dashboard sesuai OS
+# Download dashboard sesuai OS
 
-# case "$OS_ID" in
-# ubuntu)
-# DASHBOARD_URL="https://raw.githubusercontent.com/hamidw/osimpu-cloudinit/main/vps_linux/osimpu_ubuntu.sh"
-# ;;
-# debian)
-# DASHBOARD_URL="https://raw.githubusercontent.com/hamidw/osimpu-cloudinit/main/vps_linux/osimpu_debian.sh"
-# ;;
-# # almalinux)
-# # DASHBOARD_URL="https://raw.githubusercontent.com/hamidw/osimpu-cloudinit/main/vps_linux/osimpu_almalinux.sh"
-# # ;;
-# rocky)
-# DASHBOARD_URL="https://raw.githubusercontent.com/hamidw/osimpu-cloudinit/main/vps_linux/osimpu_rocky.sh"
-# ;;
-# fedora)
-# DASHBOARD_URL="https://raw.githubusercontent.com/hamidw/osimpu-cloudinit/main/vps_linux/osimpu_fedora.sh"
-# ;;
-# opensuse*|opensuse-leap|opensuse-tumbleweed)
-# DASHBOARD_URL="https://raw.githubusercontent.com/hamidw/osimpu-cloudinit/main/vps_linux/osimpu_suse.sh"
-# ;;
-# *)
-# echo "OS tidak didukung: $OS_ID"
-# exit 0
-# ;;
-# esac
+case "$OS_ID" in
+ubuntu)
+DASHBOARD_URL="https://raw.githubusercontent.com/hamidw/osimpu-cloudinit/main/vps_linux/osimpu_ubuntu.sh"
+;;
+debian)
+DASHBOARD_URL="https://raw.githubusercontent.com/hamidw/osimpu-cloudinit/main/vps_linux/osimpu_debian.sh"
+;;
+almalinux)
+DASHBOARD_URL="https://raw.githubusercontent.com/hamidw/osimpu-cloudinit/main/vps_linux/osimpu_almalinux.sh"
+;;
+rocky)
+DASHBOARD_URL="https://raw.githubusercontent.com/hamidw/osimpu-cloudinit/main/vps_linux/osimpu_rocky.sh"
+;;
+fedora)
+DASHBOARD_URL="https://raw.githubusercontent.com/hamidw/osimpu-cloudinit/main/vps_linux/osimpu_fedora.sh"
+;;
+opensuse*|opensuse-leap|opensuse-tumbleweed)
+DASHBOARD_URL="https://raw.githubusercontent.com/hamidw/osimpu-cloudinit/main/vps_linux/osimpu_suse.sh"
+;;
+*)
+echo "OS tidak didukung: $OS_ID"
+exit 0
+;;
+esac
 
-# curl -fsSL "$DASHBOARD_URL" -o /etc/profile.d/osimpu.sh
-# chmod +x /etc/profile.d/osimpu.sh
+curl -fsSL "$DASHBOARD_URL" -o /etc/profile.d/osimpu.sh
+chmod +x /etc/profile.d/osimpu.sh
 
-# # touch /root/.hushlogin
+touch /root/.hushlogin
 
-# systemctl restart ssh 2>/dev/null || true
-# systemctl restart sshd 2>/dev/null || true
+systemctl restart ssh 2>/dev/null || true
+systemctl restart sshd 2>/dev/null || true
 
 echo "Selesai."
